fd7efa663c6600b612a9e5c7963bcea767291ee4
Kirill Tolkachev <lavcraft@gmail.com>
2018-05-03 01:26:16 +0300

    Add publish logic for report.test plugin

--- src/integTest/groovy/ru/alfalab/gradle/platform/docs/reports/StacksTestReportPublishPluginProjectSpec.groovy
+++ src/integTest/groovy/ru/alfalab/gradle/platform/docs/reports/StacksTestReportPublishPluginProjectSpec.groovy
@@ -2,7 +2,10 @@ package ru.alfalab.gradle.platform.docs.reports
 
 import nebula.test.PluginProjectSpec
 import org.gradle.api.tasks.bundling.Zip
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
 import ru.alfalab.gradle.platform.stack.documentation.report.StacksTestReportsPublishPlugin
+
+import static ru.alfalab.gradle.platform.stack.documentation.report.StacksTestReportsPublishPlugin.STACKS_TEST_REPORTS_ARCHIVE_CONFIGURATION
 
 /**
  * @author tolkv
@@ -20,7 +23,28 @@ class StacksTestReportPublishPluginProjectSpec extends PluginProjectSpec {
       reportsZipTask.classifier == 'testReport'
       reportsZipTask.inputs.files.findAll { it.path.contains '/reports/tests/test' }
       reportsZipTask.dependsOn.findAll { it.name == 'test' }.size() == 1
+  }
 
+  def 'should configure artifactory tasks for publish reports'() {
+    when:
+      project.plugins.apply 'java'
+      project.plugins.apply 'com.jfrog.artifactory'
+      project.plugins.apply getPluginName()
+
+    then: 'configure publish archive with custom name and classifier'
+      project.configurations.findByName(STACKS_TEST_REPORTS_ARCHIVE_CONFIGURATION).artifacts.findAll {
+        it.name == 'should-configure-artifactory-tasks-for-publish-reports' && it.classifier == 'testReport'
+      }.size() == 1
+
+    and: 'configure artifactory task'
+      def task = project.tasks.findByName('artifactoryPublish') as ArtifactoryTask
+
+      !task.skip
+      !task.publishIvy
+
+      task.publishConfigs.findAll {
+        it.name == STACKS_TEST_REPORTS_ARCHIVE_CONFIGURATION
+      }.size() == 1
   }
 
   @Override
--- src/main/groovy/ru/alfalab/gradle/platform/stack/documentation/report/StacksTestReportsPublishPlugin.groovy
+++ src/main/groovy/ru/alfalab/gradle/platform/stack/documentation/report/StacksTestReportsPublishPlugin.groovy
@@ -1,16 +1,22 @@
 package ru.alfalab.gradle.platform.stack.documentation.report
 
+import org.gradle.api.artifacts.ConfigurablePublishArtifact
+import org.gradle.api.artifacts.dsl.ArtifactHandler
 import org.gradle.api.file.CopySpec
 import org.gradle.api.plugins.PluginContainer
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.bundling.Zip
 import org.gradle.api.tasks.testing.Test
+import org.jfrog.gradle.plugin.artifactory.ArtifactoryPlugin
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
 import ru.alfalab.gradle.platform.stack.api.PluginContainerAware
 import ru.alfalab.gradle.platform.stack.api.TaskContainerAware
 import ru.alfalab.gradle.platform.stack.base.StacksAbstractPlugin
 
 class StacksTestReportsPublishPlugin extends StacksAbstractPlugin implements PluginContainerAware, TaskContainerAware {
-  public static final String STACKS_TEST_REPORT_ARCHIVE_TASKNAME = 'stacksTestReportDistZip'
+  public static final String STACKS_TEST_REPORTS_ARCHIVE_CONFIGURATION = 'stacksTestReportsArchive'
+  public static final String STACKS_TEST_REPORT_ARCHIVE_TASKNAME       = 'stacksTestReportDistZip'
+  public static final String TEST_REPORT_CLASSIFIER                    = 'testReport'
 
   PluginContainer pluginContainer
   TaskContainer   taskContainer
@@ -23,11 +29,28 @@ class StacksTestReportsPublishPlugin extends StacksAbstractPlugin implements Plu
       if (testTask?.reports?.html?.destination) {
         stacksTestReportDistZipTask.with { CopySpec copySpec ->
           inputs.files testTask.outputs
-          classifier = 'testReport'
+          classifier = TEST_REPORT_CLASSIFIER
           copySpec.from(testTask.reports.html.destination)
         }
 
         stacksTestReportDistZipTask.dependsOn(testTask)
+
+        pluginContainer.withType(ArtifactoryPlugin) {
+          configurations.maybeCreate(STACKS_TEST_REPORTS_ARCHIVE_CONFIGURATION)
+          artifacts { ArtifactHandler artifactHandler ->
+            artifactHandler.add(STACKS_TEST_REPORTS_ARCHIVE_CONFIGURATION, stacksTestReportDistZipTask) {
+              ConfigurablePublishArtifact configurablePublishArtifact ->
+                configurablePublishArtifact.classifier = TEST_REPORT_CLASSIFIER
+            }
+          }
+        }
+
+        taskContainer.withType(ArtifactoryTask) { ArtifactoryTask task ->
+          task.skip = false
+          task.publishIvy = false
+          task.publishConfigs(STACKS_TEST_REPORTS_ARCHIVE_CONFIGURATION)
+        }
+
       } else {
         logger.warn 'stacks plugin can\'t configure stacksTestReportDistZip task. Html reports not found'
       }
